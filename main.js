8643969b5c748bfc3b2e72baece34825b2b78b5d
Arthur Ish <arthurishdev@gmail.com>
2024-04-22 20:53:04 -0700

    liked

--- main.js
+++ main.js
@@ -3,9 +3,45 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let glyphs = document.getElementsByClassName("like-glyph");
+for (let heart of glyphs){
+  heart.addEventListener("click", glyphClicked);
+}
+
+function glyphClicked(clickEvent){
+
+    mimicServerCall().catch(displayError)
+    .then((response) => {
+      if(response === "Pretend remote server notified of action!"){
+        heartSwitch(clickEvent);
+      }
+    });
 
 
+}
 
+function displayError(rejected){
+  let p = document.getElementById("modal-message");
+  p.textContent = ``;
+  let t = document.createTextNode(rejected);
+  p.appendChild(t);
+  document.getElementById("modal").classList.remove("hidden");
+
+  setTimeout(() => {document.getElementById("modal").classList.add("hidden");}, 3000);
+}
+
+function heartSwitch(e){
+  //console.log(e.target);
+  if (e.target.textContent === EMPTY_HEART){
+  e.target.textContent = FULL_HEART;
+  e.target.classList.add("activated-heart");
+  }
+  else if(e.target.textContent === FULL_HEART){
+    e.target.textContent = EMPTY_HEART;
+    e.target.classList.remove("activated-heart");
+  }
+
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
